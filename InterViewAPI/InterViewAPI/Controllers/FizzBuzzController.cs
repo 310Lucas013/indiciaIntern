017fd251e7dd78772e9191a2804fcf637bd65cfa
Vermeer,Luuk L.L.H <l.vermeer@student.fontys.nl>
2019-11-19 18:11:38 +0100

    Assignment One Finished, Working on Assignment Two, but struggling

---
+++ InterViewAPI/InterViewAPI/Controllers/FizzBuzzController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterViewAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FizzBuzzController : ControllerBase
+    {
+        //// GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET fizzbuzz/values/5
+        [HttpGet]
+        public ActionResult<List<string>> Get(int from, int to)
+        {
+            List<string> result = new List<string>();
+            for (int i = from; i <= to; i++)
+            {
+                String value = "";
+                if (i % 3 == 0)
+                {
+                    value += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    value += "Buzz";
+                }
+
+                if (value == "")
+                {
+                    value = i.ToString();
+                }
+
+                result.Add(value);
+            }
+            
+            return result;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ InterViewAPI/InterViewAPI/Controllers/FormateerController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterViewAPI.Controllers
+{
+    [Route("[controller]/sommatie")]
+    [ApiController]
+    public class FormateerController : ControllerBase
+    {
+        // GET: api/Formateer
+        [HttpGet]
+        public string Get()
+        {
+            string pattern = @"\p{Sc}*(\s?\d+[.,]?\d*)\p{Sc}*";
+            string replacement = "$1";
+            string input = "";
+            foreach (object obj in InputValues())
+            {
+                if (obj != null)
+                {
+                    input += ojb
+                }
+            }
+            //string input = InputValues().ToString();
+            return input;
+        }
+
+        // GET: api/Formateer/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Formateer
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Formateer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private static object[] InputValues()
+
+        {
+
+            return new[]
+
+            {
+
+                "5",
+
+                "1,2e2",
+
+                null,
+
+                " -5555",
+
+                "6.767"
+
+            };
+
+        }
+    }
+}
